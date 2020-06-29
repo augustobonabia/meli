9f1f64aa353254c91de33dff8cb658dc02fb05e4
abonabia <augusto.bonabia@gmail.com>
2020-06-29 20:36:46 -0300

    api - mejora en el consumo de axios

--- api/src/core/source-api-client.js
+++ api/src/core/source-api-client.js
@@ -2,16 +2,12 @@ const axios = require('axios');
 const env = require('../environment');
 const log = require('../logger');
 
-const { sourceApiBaseUrl } = env;
+const baseURL = env.sourceApiBaseUrl;
 
 async function get(path) {
-  const url = `${sourceApiBaseUrl}/${path}`;
+  const response = (await axios.get(path, { baseURL })).data;
 
-  log.info(`GET: ${url}`);
-
-  const response = (await axios.get(url)).data;
-
-  const responseLogObj = { method: 'GET', url };
+  const responseLogObj = { method: 'GET', baseURL, path };
   if (env.logRequestObjsOnError) {
     responseLogObj.response = response;
   }
