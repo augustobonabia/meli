e3b898825283991d57370df4d68e1c86d9d253c0
abonabia <augusto.bonabia@gmail.com>
2020-07-02 04:00:43 -0300

    front-end -  arreglo webpack.config.js para que compile los archivos .js

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -7,7 +7,7 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         options: { presets: ['@babel/env'] },
