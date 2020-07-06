56e449005a64fe240c41d9416fe1620fc95e6d16
abonabia <augusto.bonabia@gmail.com>
2020-07-06 01:44:02 -0300

    api - agrego webfontloader y configuro 2 de las familias de fuentes principales

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import WebFont from 'webfontloader';
 import App from './core/App';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+WebFont.load({
+  google: {
+    families: ['Proxima Nova:300,400,700', 'Roboto:300,400,700'],
+  },
+});
