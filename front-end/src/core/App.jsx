872ca27434debb437a656c7f51f93a65582c8331
abonabia <augusto.bonabia@gmail.com>
2020-07-08 22:12:35 -0300

    front-end - agrego un key al ErrorBoundary para forzar a que los componentes se recreen al cambiar de ubicación

--- front-end/src/core/App.jsx
+++ front-end/src/core/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 import './app.scss';
 import appRoutes from './app-routes';
 import Header from '../Header';
@@ -9,11 +9,13 @@ import ItemPage from '../ItemPage';
 import ErrorBoundary from '../shared-componets/ErrorBoundary';
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="app">
       <Header />
       <Switch>
-        <ErrorBoundary>
+        <ErrorBoundary key={location.path}>
           <Route exact path={appRoutes.searchBox.path} />
           <Route exact path={appRoutes.itemsList.path}>
             <ItemsListPage />
